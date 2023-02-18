45f1b1d2d280da099b9fc99832b77339f4edfcca
BABANGIDA ZACHARIAH <44602491+babangidazachariah@users.noreply.github.com>
2023-02-17 23:45:52 -0500

    Update createDatabase.php

--- createDatabase.php
+++ createDatabase.php
@@ -1,10 +1,15 @@
 <?php
-mysql://b35bfc72e5de73:d0de149c@us-cdbr-east-06.cleardb.net/heroku_b1434386a052247?reconnect=true
-    $con = mysqli_connect("heroku_b1434386a052247","b35bfc72e5de73","d0de149c" ); //or die ('Unable to connect. Check your connection parameters or '.mysql_error($db));
-    $sql= "CREATE DATABASE IF NOT EXISTS HyamdaDatabase";
-    mysqli_query($sql, $con); // or die(mysql_error($con));
+    //mysql://b35bfc72e5de73:d0de149c@us-cdbr-east-06.cleardb.net/heroku_b1434386a052247?reconnect=true
+    $con = new mysqli("heroku_b1434386a052247","b35bfc72e5de73","d0de149c","heroku_b1434386a052247" ); //or die ('Unable to connect. Check your connection parameters or '.mysql_error($db));
+    if($con->con_errno){
+
+        print("Failed to connect to MySQL: " . $con->connect_error);
+        exit();
+    }
+    //$sql= "CREATE DATABASE IF NOT EXISTS HyamdaDatabase";
+    //$con->mysqli_query($sql); 
     //make sure our recently created database is the active one
-    mysqli_select_db('HyamdaDatabase', $db); //or die(mysql_error($db));
+    //mysqli_select_db('HyamdaDatabase', $db); 
     
     $sql = "CREATE TABLE IF NOT EXISTS tbltesWords (wordID INTEGER NOT NULL AUTO_INCREMENT,														
                                                         word VARCHAR(50) NOT NULL,
