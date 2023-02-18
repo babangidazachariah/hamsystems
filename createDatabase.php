ed70f2e2bddca08e72ab8a22905ba80f2c9664d2
BABANGIDA ZACHARIAH <44602491+babangidazachariah@users.noreply.github.com>
2023-02-17 23:48:06 -0500

    Update createDatabase.php

--- createDatabase.php
+++ createDatabase.php
@@ -1,11 +1,5 @@
 <?php
-    //mysql://b35bfc72e5de73:d0de149c@us-cdbr-east-06.cleardb.net/heroku_b1434386a052247?reconnect=true
-    $con = new mysqli("heroku_b1434386a052247","b35bfc72e5de73","d0de149c","heroku_b1434386a052247" ); //or die ('Unable to connect. Check your connection parameters or '.mysql_error($db));
-    if($con->con_errno){
-
-        print("Failed to connect to MySQL: " . $con->connect_error);
-        exit();
-    }
+    require_once("connection.php");
     //$sql= "CREATE DATABASE IF NOT EXISTS HyamdaDatabase";
     //$con->mysqli_query($sql); 
     //make sure our recently created database is the active one
