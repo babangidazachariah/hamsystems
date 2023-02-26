0e530507a10da32dd4cf9fe0e9ec654ae6906de6
BABANGIDA ZACHARIAH <44602491+babangidazachariah@users.noreply.github.com>
2023-02-25 22:07:57 -0500

    Add files via upload

---
+++ sendEmail.php
@@ -0,0 +1,39 @@
+<?php
+require 'PHPMailerAutoload.php';
+print(DIRECTORY_SEPARATOR.'connection.php');
+$mail = new PHPMailer;
+
+$mail->SMTPDebug = 4;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'kyaamhyam@gmail.com';                 // SMTP username
+$mail->Password = 'dwsumznnivsfcaum';                           // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                    // TCP port to connect to
+
+$mail->setFrom('kyaamhyam@gmail.com', 'Hyam Learning Platform');
+$mail->addAddress('daddonyone@gmail.com');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+$mail->addReplyTo('kyaamhyam@gmail.com');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Here is the subject';
+$mail->Body    = '<h1>This is the HTML</h1> message body <b>in bold!</b>';
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+}
+
+
+?>
